20350cbada5483d9702838f2a0ed2e3da2db31df
sankarshan <sankarshan7@gmail.com>
2019-04-27 16:34:33 +0530

    Add function

--- pysank/pysank.py
+++ pysank/pysank.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
-
 """Main module."""
+
+import os
+
+def create_directory(directory_name):
+    """Create directory safely after checking if it already 
+    exists or not.
+    
+    Arguments:
+        directory_name {str} -- name of the directory 
+    """
+    if os.path.exists(directory_name):
+        print("Directory: {}/ already exists.".format(directory_name))
+    else:
+        os.makedirs(directory_name)
+        print("Directory: {}/ created.".format(directory_name))
+
