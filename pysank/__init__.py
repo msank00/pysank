378ee3fd0866fc8975096eb83a2b80be94a63f00
sankarshan <sankarshan7@gmail.com>
2019-05-06 20:46:47 +0530

    Format with black [pep8]

--- pysank/__init__.py
+++ pysank/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for pysank."""
 
 __author__ = """Sankarshan Mridha"""
-__email__ = 'sankarshan7@gmail.com'
-__version__ = '0.1.12'
+__email__ = "sankarshan7@gmail.com"
+__version__ = "0.1.12"
--- setup.py
+++ setup.py
@@ -5,51 +5,47 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ["Click>=6.0"]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author="Sankarshan Mridha",
-    author_email='sankarshan7@gmail.com',
+    author_email="sankarshan7@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="personal utility file for data science",
-    entry_points={
-        'console_scripts': [
-            'pysank=pysank.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["pysank=pysank.cli:main"]},
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='pysank',
-    name='pysank',
-    packages=find_packages(include=['pysank']),
+    keywords="pysank",
+    name="pysank",
+    packages=find_packages(include=["pysank"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/msank00/pysank',
-    version='0.1.12',
+    url="https://github.com/msank00/pysank",
+    version="0.1.12",
     zip_safe=False,
 )
