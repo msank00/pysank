33d071f1aa08b1487c1a02b87c220966f5f2b3d1
sankarshan <sankarshan7@gmail.com>
2020-02-12 22:29:27 +0530

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/msank00/pysank",
-    version="version='0.3.0'",
+    version="version='0.4.0'",
     zip_safe=False,
 )
